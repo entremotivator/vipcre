b16baceff12a996a26d2e1406225f0e005cd619e
entremotivator <104661532+entremotivator@users.noreply.github.com>
2024-08-26 19:49:15 -0400

    Update sign.py

--- pages/sign.py
+++ pages/sign.py
@@ -4,9 +4,9 @@ from wordpress_auth import WordpressAuth
 def get_user_input():
     with st.sidebar:
         st.title("Configuration Settings")
-        base_url = st.text_input("WordPress Site URL", placeholder="https://vipbusinesscredit.com/")
-        api_key = st.text_input("API Key", type="password")
-        return base_url, api_key
+        wordpress_url = st.text_input("WordPress Site URL", placeholder="https://yourwordpressurl.com")
+        api_key_input = st.text_input("API Key", type="password")
+        return wordpress_url, api_key_input
 
 def main_page():
     st.title("Welcome to the Application")
@@ -16,12 +16,12 @@ def main_page():
 def login_page(auth):
     st.title("Please Log In")
     with st.form(key='login_form'):
-        username = st.text_input('Username')
-        password = st.text_input('Password', type='password')
+        username_input = st.text_input('Username')
+        password_input = st.text_input('Password', type='password')
         submit_button = st.form_submit_button(label='Log In')
 
         if submit_button:
-            token = auth.get_token(username, password)
+            token = auth.get_token(username_input, password_input)
             if token and auth.verify_token(token):
                 st.session_state['token'] = token  # Store the token in the session state
                 st.success("Logged in successfully!")
@@ -29,12 +29,12 @@ def login_page(auth):
             else:
                 st.error('Access denied. Please check your credentials and try again.')
 
-# Sidebar for configuration
-base_url, api_key = get_user_input()
+# Sidebar for configuration inputs
+wordpress_url, api_key_input = get_user_input()
 
-if base_url and api_key:
+if wordpress_url and api_key_input:
     # Initialize the WordPressAuth instance
-    auth = WordpressAuth(api_key=api_key, base_url=base_url)
+    auth = WordpressAuth(api_key=api_key_input, base_url=wordpress_url)
     
     # Check if the user is already logged in
     if 'token' in st.session_state and auth.verify_token(st.session_state['token']):
